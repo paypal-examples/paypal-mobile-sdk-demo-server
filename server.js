a82218922a9e20e1757576ed6320228ce1dfd7fb
Victoria Park <vicpark@paypal.com>
2025-01-02 07:02:43 -0800

    get and capture endpoints

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Client, Environment } = require('@paypal/paypal-server-sdk');
+const { Client, Environment, OrdersController } = require('@paypal/paypal-server-sdk');
 
 const app = express();
 const sdkClient = new Client({
@@ -10,10 +10,35 @@ const sdkClient = new Client({
     environment: Environment.Sandbox,
 });
 
+const ordersController = new OrdersController(sdkClient);
+
 app.get('/ping', (req, res) => {
     res.type('text/plain');
     res.send('pong')
 })
+
+app.get('/orders/:orderID', async (req, res, next) => {
+    try {
+        const response = await ordersController.ordersGet({
+            id: req.params.orderID
+        })
+        res.json(response.body)
+    } catch  (err) {
+        next(err)
+    }
+});
+
+app.post('/orders/:orderID/capture', async (req, res, next) => {
+    try {
+        const response = await ordersController.ordersCapture({
+            id: req.params.orderID,
+            paypalClientMetadataId: req.header('PayPal-Client-Metadata-Id')
+        })
+        res.json(response.body)
+    } catch  (err) {
+        next(err)
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
